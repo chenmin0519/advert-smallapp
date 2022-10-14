9e6d6c7fc06dd0b567a9782e0166c5a782221883
min <18874830336@163.com>
2022-10-14 15:29:37 +0800

    文件破解爬虫

--- src/main/java/com/advert/smallapp/task/FileCheckTask.java
+++ src/main/java/com/advert/smallapp/task/FileCheckTask.java
@@ -1,15 +1,25 @@
 package com.advert.smallapp.task;
 
-import org.apache.commons.lang3.time.DateUtils;
+import com.advert.smallapp.utils.DahuZhongxueFileUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.text.ParseException;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Configuration      //1.主要用于标记配置类，兼备Component的效果。
 @EnableScheduling   // 2.开启定时任务
+@Slf4j
 public class FileCheckTask {
 
+//    @Scheduled(cron = "0/5 * * * * ?")
+    @Scheduled(cron = "0 0 10,16 * * ?")
+    private void configureTasks() throws ParseException {
+        log.info("开始检查达浒中学新文件");
+        DahuZhongxueFileUtils.checkDhzxFile();
+        log.info("检查达浒中学文件完成");
+    }
 
 }
